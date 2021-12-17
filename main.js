a663d23bbc1fad8be2e2e4143d7cf78ec65a9db4
Skanler <83680873+Skanler@users.noreply.github.com>
2021-12-16 20:43:28 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+//https://teachablemachine.withgoogle.com/models/rEJhyHWm9/
+function startclassification(){
+navigator.mediaDevices.getUserMedia({audio: true});
+classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/rEJhyHWm9/model.json', modelReady);
+}
+function modelReady(){
+    classifier.classify(gotResults);
+}
